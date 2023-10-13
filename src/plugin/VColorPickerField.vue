a5b27f34b233f61d3addb98eaecda17eb63257d3
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-10-12 18:21:38 -0700

    Building main functionality

--- src/plugin/VColorPickerField.vue
+++ src/plugin/VColorPickerField.vue
@@ -1,5 +1,8 @@
 <template>
-	<div ref="fieldContainerRef">
+	<div
+		ref="fieldContainerRef"
+		class="v-color-picker-field"
+	>
 		<!-- Dot Option -->
 		<div
 			v-if="dotField"
@@ -46,24 +49,82 @@
 		<v-text-field
 			v-else
 			ref="textFieldRef"
-			v-model="modelValue"
+			v-bind="$attrs"
 			:class="textFieldClasses"
+			:color="color"
 			:density="density"
 			:hint="hint"
+			:model-value="modelValue"
 			:persistent-hint="persistentHint"
 			:readonly="readonly"
-			:value="modelValue"
-			v-bind="$attrs"
-			@click="toggleColorPicker"
+			@click:control="toggleColorPicker('textField')"
 			@mouseenter="closePickerStop"
 			@mouseleave="closePicker"
-			@update:model-value="updateModelValue"
+			@update:model-value="updateModelValue($event, 'textField')"
 		>
+			<template
+				v-for="(_, slot) in slots"
+				#[slot]="scope"
+			>
+				<slot
+					v-if="!slots['prepend']"
+					:name="slot"
+					v-bind="{ ...scope }"
+				/>
+			</template>
+
+			<template
+				v-if="prependIcon && !slots['prepend']"
+				#prepend
+			>
+				<ColorPickerIcon
+					:color="hoverIconColor"
+					:icon="prependIcon"
+					@click="toggleColorPicker"
+				/>
+			</template>
+
+			<template
+				v-if="prependInnerIcon && !slots['prepend-inner']"
+				#prepend-inner
+			>
+				<ColorPickerIcon
+					:color="hoverIconColor"
+					:icon="prependInnerIcon"
+				/>
+				<!-- @click="toggleColorPicker" -->
+			</template>
+
+			<template
+				v-if="appendIcon && !slots['append']"
+				#append
+			>
+				<ColorPickerIcon
+					:color="hoverIconColor"
+					:icon="appendIcon"
+					@click="toggleColorPicker"
+				/>
+			</template>
+
+			<template
+				v-if="appendInnerIcon && !slots['append-inner']"
+				#append-inner
+			>
+				<ColorPickerIcon
+					:color="hoverIconColor"
+					:icon="appendInnerIcon"
+				/>
+				<!-- @click="toggleColorPicker" -->
+			</template>
+
+			<!-- @update:model-value="updateModelValue" -->
+
 			<!-- Label Slot -->
 			<template
 				v-if="slots.label || label"
 				#label
 			>
+
 				<slot
 					v-if="slots.label"
 					name="label"
@@ -81,39 +142,51 @@
 
 	<!-- Card Picker Container  -->
 	<Teleport to="body">
-		<v-card
-			v-if="colorPickerOpen"
-			v-bind="cardProps"
-			class="v-color-picker-card"
-			:style="cardStyles"
-			:width="textFieldProperties.width"
-			@mouseenter="closePickerStop"
-			@mouseleave="closePicker"
-		>
-			<v-card-text
-				class="d-flex justify-center"
-				:style="cardTextStyles"
+		<div class="position-elm-helper"></div>
+
+		<v-defaults-provider :defaults="defaults">
+			<v-card
+				v-if="colorPickerOpen"
+				class="v-color-picker-field-card"
+				:style="cardStyles"
+				:width="textFieldProperties.width"
+				@mouseenter="closePickerStop"
+				@mouseleave="closePicker"
 			>
 				<v-color-picker
-					v-bind="colorPickerProps"
-					v-model="modelValue"
+					v-model="colorPickerModelValue"
 					class="v-color-selection"
-					:hide-mode-switch="true"
-					:width="textFieldProperties.width"
+					:mode="pickerMode"
+					@update:mode="updateMode"
+					@update:model-value="updateModelValue($event, 'colorPicker')"
 				></v-color-picker>
 				<!-- @update:model-value="updateModelValue" -->
-			</v-card-text>
-		</v-card>
+			</v-card>
+		</v-defaults-provider>
 	</Teleport>
 </template>
 
 <script setup lang="ts">
-import type {
-	VCard,
-	VColorPicker,
-	VTextField,
-} from 'vuetify/components';
+import {
+	HtmlRefElement,
+	Mode,
+	Props,
+	TextFieldProperties,
+	VuetifyDefaults,
+} from '@/types';
 import { useConvertToUnit } from '@/plugin/composables/helpers';
+import {
+	useDotContainerClass,
+	useDotFieldClasses,
+	useHintClasses,
+	useTextFieldClasses,
+} from '@/plugin/composables/classes';
+import {
+	useDotStyles,
+	useHintStyles,
+} from '@/plugin/composables/styles';
+import ColorPickerIcon from '@/plugin/components/ColorPickerIcon.vue';
+
 
 defineOptions({
 	inheritAttrs: false,
@@ -123,49 +196,15 @@ const attrs = useAttrs();
 const slots = useSlots();
 const emit = defineEmits(['update:modelValue']);
 
-// -------------------------------------------------- Props #
-interface Props {
-	cardProps?: VCard['$props'] & {
-		align?: string;
-		borderColor?: string;
-		borderStyle?: string;
-		borderWidth?: number;
-		closeDelay?: number;
-		direction?: string;
-		fieldOffset?: number;
-		padding?: number;
-		verticalOffset?: number;
-	};
-	colorPickerProps?: VColorPicker['$props'] & {
-		hideModeSwitch?: boolean;
-	};
-	defaultColor?: string;
-	density?: VTextField['$props']['density'];
-	dotField?: boolean;
-	dotFieldProps?: {
-		borderColor?: string;
-		borderRadius?: string;
-		borderStyle?: string;
-		borderWidth?: number;
-		cursor?: string;
-		height?: string | number;
-		width?: string | number;
-	};
-	hint?: string;
-	hintAlign?: 'left' | 'right';
-	hintColor?: string;
-	label?: string;
-	name?: any;
-	persistentCard?: boolean;
-	persistentHint?: boolean;
-	readonly?: boolean;
-	required?: boolean;
-}
 
+// -------------------------------------------------- Props //
 const props = withDefaults(defineProps<Props>(), {
+	// appendIcon: 'mdi:mdi-palette',
+	// appendInnerIcon: 'mdi:mdi-palette',
 	cardProps: () => ({}) as const,
+	color: undefined,
 	colorPickerProps: () => ({}) as const,
-	defaultColor: '#ff0000',
+	defaultColor: '',
 	density: 'default',
 	dotField: false,
 	dotFieldProps: () => ({
@@ -180,62 +219,77 @@ const props = withDefaults(defineProps<Props>(), {
 	hint: '',
 	hintAlign: 'left',
 	hintColor: 'default',
+	iconHoverColor: undefined,
 	label: '',
 	name: 'color',
 	persistentCard: false,
 	persistentHint: false,
+	// prependIcon: 'mdi:mdi-palette',
+	prependInnerIcon: 'mdi:mdi-palette',
 	readonly: false,
 	required: false,
 });
 
-// ------------------------- VCard #
+
+// -------------------------------------------------- Defaults //
+// ------------------------- VCard //
 const defaultCardProps: Props['cardProps'] = {
-	align: 'left',
+	align: 'bottom left',
 	border: false,
 	borderColor: 'transparent',
 	borderStyle: 'solid',
 	borderWidth: 2,
 	closeDelay: 500,
 	color: '',
-	direction: 'below',
 	elevation: 5,
 	fieldOffset: props.dotField ? 0 : 10,
+	fullWidth: false,
 	hover: false,
 	loading: false,
-	padding: 0,
+	padding: 4,
 	ripple: false,
 	tag: 'div',
 	verticalOffset: 28,
 };
 
-const cardProps = computed<Props['cardProps']>(() => ({
-	...defaultCardProps,
-	...props.cardProps,
-}));
-
-// ------------------------- VColorPicker #
+// ------------------------- VColorPicker //
 const defaultColorPickerProps: Props['colorPickerProps'] = {
 	elevation: 0,
 	hideModeSwitch: true,
-	mode: 'hex',
+	// mode: 'hex',
+	// modes: ['hex'],
 };
 
-const colorPickerProps = computed<Props['colorPickerProps']>(() => ({
-	...defaultColorPickerProps,
-	...props.colorPickerProps,
-}));
+const defaults = ref<VuetifyDefaults>({
+	VCard: {
+		...defaultCardProps,
+		...props.cardProps,
+	},
+	VColorPicker: {
+		...defaultColorPickerProps,
+		...props.colorPickerProps,
+	},
+	// global: {	},
+});
+
+// console.log('defaults', defaults.value);
 
 
 // -------------------------------------------------- Data #
 const cardStyles = ref({});
-const colorPickerOpen = ref(false);
-const dotFieldRef = ref<HTMLDivElement | null>(null);
-const dotHintActive = ref(false);
-const fieldContainerRef = ref<HTMLDivElement | null | undefined>(null);
-const modelValue = ref<string>(attrs.modelValue as string);
-let textFieldProperties = reactive({
+const colorPickerOpen = ref<boolean>(false);
+const dotFieldRef = ref<HtmlRefElement>(null);
+const dotHintActive = ref<boolean>(false);
+const fieldContainerRef = ref<HtmlRefElement>(null);
+const colorPickerModelValue = ref<any>(attrs.modelValue);
+const modelValue = ref<any>(attrs.modelValue);
+const pickerMode = ref<Mode>(defaults.value.VColorPicker?.mode);
+let textFieldProperties = reactive<TextFieldProperties>({
+	bottom: 0,
 	height: 0,
 	left: 0,
+	// maxWidth: 'auto',
+	right: 0,
 	top: 0,
 	width: 0,
 });
@@ -245,110 +299,50 @@ const { dotField, dotFieldProps } = toRefs(props);
 
 // -------------------------------------------------- Computed #
 // ------------------------- Dot Selector #
-const dotContainerClass = computed<string>(() => {
-	let classes = 'v-input--horizontal ';
+const dotContainerClass = computed(() => useDotContainerClass({
+	density: props.density,
+}));
 
-	if (props.density === 'compact') {
-		classes += 'v-input--density-compact';
-	}
+const dotFieldClasses = computed(() => useDotFieldClasses({
+	name: props.name,
+}));
 
-	if (props.density === 'default') {
-		classes += 'v-input--density-default';
-	}
-
-	if (props.density === 'comfortable') {
-		classes += 'v-input--density-comfortable';
-	}
-	return classes;
-});
-
-const dotFieldClasses = computed<string>(() => {
-	let classes = 'v-field__field ';
-	classes += 'v-color-picker--dot-field ';
-	classes += `v-color-picker--dot-field-${props.name}`;
-
-	return classes;
-});
-
-const dotStyles = computed(() => {
-	const dotOptions = dotFieldProps.value;
-	let styles = {};
-
-	if (dotField.value) {
-		styles = {
-			backgroundColor: modelValue.value,
-			borderColor: dotOptions.borderColor,
-			borderRadius: useConvertToUnit({ value: dotOptions.borderRadius }),
-			borderStyle: dotOptions.borderStyle,
-			borderWidth: useConvertToUnit({ value: dotOptions.borderWidth }),
-			cursor: dotOptions.cursor,
-			height: useConvertToUnit({ value: dotOptions.height }),
-			width: useConvertToUnit({ value: dotOptions.width }),
-		};
-	}
-
-	return styles as CSSProperties;
-});
+const dotStyles = computed(() => useDotStyles({
+	dotField,
+	dotFieldProps,
+	modelValue,
+}));
 
 // ------------------------- Text Field #
-const textFieldClasses = computed(() => {
-	const classes = {
-		'v-color-picker--text-field': true,
-		'v-color-picker--text-field-readonly': props?.readonly ?? false,
-		[`v-color-picker--text-field-${props.name}`]: true,
-	};
+const textFieldClasses = computed(() => useTextFieldClasses({
+	name: props.name,
+	readonly: props.readonly,
+}));
 
-	return classes;
+const hoverIconColor = computed<string | undefined>(() => {
+	if (props.iconHoverColor === false) {
+		return undefined;
+	}
+
+	if (typeof props.iconHoverColor === 'string') {
+		return props.iconHoverColor;
+	}
+
+	return props.color ?? undefined;
 });
 
 // ------------------------- Hint #
-const hintClasses = computed(() => {
-	const classes = {
-		'text-left': props.hintAlign === 'left',
-		'text-right': props.hintAlign === 'right',
-	};
+const hintClasses = computed(() => useHintClasses({
+	hintAlign: props.hintAlign,
+	hintColor: props.hintColor,
+}));
 
-	if (props.hintColor) {
-		classes[`text-${props.hintColor}`] = true;
-	}
-
-	return classes;
-});
-
-const hintStyles = computed(() => {
-	const styles: {
-		color?: string;
-		textAlign?: string;
-		transform?: string;
-	} = {
-		color: props.hintColor,
-	};
-
-	if (!props?.persistentHint && !dotHintActive.value) {
-		styles.transform = 'translateY(-20px)';
-	}
-
-	if (props.hintAlign === 'left') {
-		styles.textAlign = 'text-left';
-	}
-
-	if (props.hintAlign === 'right') {
-		styles.textAlign = 'text-right';
-	}
-
-	return styles;
-});
-
-// ------------------------- Card #
-const cardTextStyles = computed(() => {
-	const styles: {
-		padding?: string | number | undefined;
-	} = {
-		padding: useConvertToUnit({ value: cardProps.value?.padding }),
-	};
-
-	return styles as CSSProperties;
-});
+const hintStyles = computed(() => useHintStyles({
+	dotHintActive,
+	hintAlign: props.hintAlign,
+	hintColor: props.hintColor,
+	persistentHint: props.persistentHint,
+}));
 
 
 // -------------------------------------------------- Methods //
@@ -361,129 +355,206 @@ function closePicker() {
 	console.log('closePicker');
 }
 
-function toggleColorPicker(): void {
-	// const fieldClassName = `v-color-picker--${type}-field-${props.name}`;
-
-	// Determine if the field is a dot field or a text field //
+// ------------------------- Toggle Color Picker //
+function toggleColorPicker(e, trigger = 'na'): void {
+	console.log('trigger', trigger, e);
+	const defaultCoords = { left: 0, right: 0, top: 0, width: 0 };
 	let fieldContainer = fieldContainerRef.value;
 
+	// Determine if the field is a dot field or a text field //
 	if (props.dotField) {
 		fieldContainer = dotFieldRef?.value?.querySelector('.v-input__control');
+		dotHintActive.value = !dotHintActive.value;
 	}
+
+	const fieldElementCoords = fieldContainer?.getBoundingClientRect() ?? defaultCoords;
 
 	// Set the height and width of the field //
 	let inputHeight: number | undefined = fieldContainer?.offsetHeight ?? 0;
-	let inputWidth: number | undefined = 300;
+	let inputWidth: number = 300;
+	let positionLeft = fieldElementCoords?.left ?? 0;
+	let positionRight = fieldElementCoords?.right ?? 0;
 
 	if (!props.dotField) {
-		const fieldContainerInput = fieldContainerRef?.value?.querySelector('input');
-		inputHeight = fieldContainerInput?.offsetHeight;
-		inputWidth = fieldContainerInput?.offsetWidth;
+		const fieldInputContainer: HtmlRefElement = fieldContainerRef?.value?.querySelector('.v-field__input');
+		const fieldContainerInputCoords = fieldInputContainer?.getBoundingClientRect() ?? defaultCoords;
+
+		inputHeight = fieldInputContainer?.offsetHeight;
+		inputWidth = fieldInputContainer?.offsetWidth ?? 0;
+		positionLeft = fieldContainerInputCoords.left;
+		positionRight = fieldContainerInputCoords.right - fieldContainerInputCoords.width;
 	}
 
-	const fieldElementCoords = fieldContainer?.getBoundingClientRect() ?? { left: 0, top: 0 };
-
 	textFieldProperties = {
+		bottom: 'initial',
 		height: inputHeight as number,
-		left: fieldElementCoords?.left ?? 0,
+		left: positionLeft,
+		// maxWidth: inputWidth,
+		right: positionRight,
 		top: fieldElementCoords?.top ?? 0,
-		width: inputWidth as number,
+		width: defaults.value.VCard?.fullWidth ? inputWidth : 'auto',
 	};
 
 	colorPickerOpen.value = !colorPickerOpen.value;
 	setCardStyles();
 }
 
+// ------------------------- Set Card Styles //
 function setCardStyles(): void {
-	let top = textFieldProperties.top + textFieldProperties.height;
-	let offsetTop = cardProps.value?.fieldOffset ?? 0;
+	let top: string | number = Number(textFieldProperties.top) + Number(textFieldProperties.height);
+	let bottom: string | number = 'initial';
+	let offsetTop = defaults.value.VCard?.fieldOffset ?? 0;
 
 	if (props.hint) {
-		offsetTop += cardProps.value?.verticalOffset ?? 0;
+		offsetTop += defaults.value.VCard?.verticalOffset ?? 0;
 	}
 
 	top = top + offsetTop;
+
+	// Top/Bottom Card Alignment //
+	if (defaults.value.VCard?.align?.includes('top')) {
+		bottom = (window.innerHeight - top) + Number(textFieldProperties.height) + (offsetTop * 2);
+		top = 'initial';
+	}
+
+	// Left/Right Card Alignment //
+	let left = textFieldProperties.left;
+	let right = textFieldProperties.right;
+
+	if (defaults.value.VCard?.fullWidth) {
+		left = textFieldProperties.left;
+		right = 'initial';
+	}
+	else {
+		left = textFieldProperties.left;
+		right = 'initial';
+
+		if (defaults.value.VCard?.align?.includes('right')) {
+			left = 'initial';
+			right = textFieldProperties.right;
+		}
+	}
 
 	const styles: {
 		borderColor?: string;
 		borderStyle?: string;
 		borderWidth?: string;
-		maxWidth?: string;
+		bottom?: string | number;
+		left?: string | number;
+		// maxWidth?: string | number;
 		minWidth?: string;
-		top?: string;
-		right?: string;
-		left?: string;
+		padding?: string;
+		right?: string | number;
+		top?: string | number;
+		width?: string | number;
 	} = {
-		borderColor: cardProps.value?.borderColor ?? 'transparent',
-		borderStyle: cardProps.value?.borderStyle ?? 'solid',
-		borderWidth: useConvertToUnit({ value: cardProps.value?.borderWidth ?? 0 }),
-		maxWidth: 'initial',
+		borderColor: defaults.value.VCard?.borderColor ?? 'transparent',
+		borderStyle: defaults.value.VCard?.borderStyle ?? 'solid',
+		borderWidth: useConvertToUnit({ value: defaults.value.VCard?.borderWidth ?? 0 }),
+		bottom: useConvertToUnit({ value: bottom }),
+		left: useConvertToUnit({ value: left }),
+		// maxWidth: useConvertToUnit({ value: textFieldProperties.maxWidth }),
 		minWidth: useConvertToUnit({ value: textFieldProperties.width }),
+		padding: useConvertToUnit({ value: defaults.value.VCard?.padding }),
+		right: useConvertToUnit({ value: right }),
 		top: useConvertToUnit({ value: top }),
+		width: useConvertToUnit({ value: textFieldProperties.width }),
 	};
 
-	// Default is below the input //
-	let left = textFieldProperties.left;
+	// ! DEBUG HELPER ! //
+	// const positionHelper: HTMLDivElement | null | undefined = document.querySelector('.position-elm-helper');
 
-	if (cardProps.value?.direction === 'left' || cardProps.value?.direction === 'right') {
-		styles.right = `-${styles.minWidth}`;
+	// if (positionHelper) {
+	// 	positionHelper.style.top = styles.top as string;
+	// 	positionHelper.style.left = styles.left as string;
+	// 	positionHelper.style.right = styles.right as string;
+	// 	positionHelper.style.bottom = styles.bottom as string;
+	// }
+	// ! END DEBUG HELPER ! //
 
-		if (cardProps.value.direction === 'left') {
-			left = textFieldProperties.left - textFieldProperties.width;
-		}
-
-		if (cardProps.value.direction === 'right') {
-			left = textFieldProperties.left + textFieldProperties.width;
-		}
-	}
-
-	styles.left = useConvertToUnit({ value: left });
+	console.log({ styles });
 
 	cardStyles.value = styles;
 }
 
-function updateModelValue(value: any): void {
-	console.log('updateModelValue', value);
+function updateModelValue(value: any, source = 'textField') {
+	console.log('======= updateModelValue', value, source);
 	let returnColor = value;
 
-	if (!value) {
+	if (value === '#') {
 		returnColor = props.defaultColor;
 	}
 
-	if (props?.colorPickerProps.mode === 'hex' && value.length > 7) {
-		returnColor = value.substr(0, 7);
+	if (!value) {
+		returnColor = '#';
 	}
 
-	if (value.toString().match(/#[a-zA-Z0-9]{7}/)) {
-		returnColor = value.substr(0, 7);
+	if (returnColor.length < 7) {
+		modelValue.value = returnColor;
+		return;
 	}
 
+	if (pickerMode.value === 'hex') {
+		if (value.length > 7) {
+			returnColor = value.substr(0, 7);
+		}
+
+		if (returnColor.toString().match(/#[a-zA-Z0-9]{7}/)) {
+			returnColor = value.substr(0, 7);
+		}
+	}
+
+
+	colorPickerModelValue.value = returnColor;
 	modelValue.value = returnColor;
 	emit('update:modelValue', returnColor);
+}
+
+function updateMode(mode: Mode) {
+	console.log('mode', mode);
+	pickerMode.value = mode;
+	console.log('colorPickerModelValue.value', colorPickerModelValue.value);
+	modelValue.value = colorPickerModelValue.value;
+	//
 }
 
 </script>
 
 <style lang="scss" scoped>
-.v-color-picker {
-	max-width: 100% !important;
-
+.v-color-picker-field {
 	&--dot-field {
 		overflow: visible;
 		position: relative;
 	}
 
-	&--text-field {
-		&-readonly {
-			:deep(.v-field__input) {
-				cursor: pointer !important;
-			}
-		}
-	}
+	// &--text-field {
+	// 	&-readonly {
+	// 		:deep(.v-field__input) {
+	// 			cursor: pointer !important;
+	// 		}
+	// 	}
+	// }
 
 	&-card {
 		position: absolute;
 		z-index: 999999;
+
+		.v-color-picker {
+			max-width: 100% !important;
+			width: 100% !important;
+		}
 	}
+}
+
+.position-elm-helper {
+	background-color: #f00;
+	border-radius: 50%;
+	display: block;
+	height: 10px;
+	left: 0;
+	position: absolute;
+	top: 0;
+	width: 10px;
+	z-index: 99999999999;
 }
 </style>
