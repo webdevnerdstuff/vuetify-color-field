a2ecd03a0a444e972fc9c0d1f8d218d953c10e9b
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-10-13 18:15:07 -0700

    Add new variable for classes

--- src/plugin/composables/classes.ts
+++ src/plugin/composables/classes.ts
@@ -30,11 +30,13 @@ export const useDotFieldClasses: UseDotFieldClass = (options) => {
 
 // ------------------------------------------------ Text Field //
 export const useTextFieldClasses: UseTextFieldClasses = (options) => {
-	const { name, readonly } = options;
+	const { name, readonly, readonlyInput } = options;
+	console.log(options);
 
 	return {
 		[`${componentName}--text-field-${name}`]: true,
-		[`${componentName}--text-field-${readonly}`]: readonly ?? false,
+		[`${componentName}--text-field-readonly`]: readonly ?? false,
+		[`${componentName}--text-field-readonly-input`]: readonlyInput && !readonly ? true : false,
 		[`${componentName}--text-field`]: true,
 	};
 };
