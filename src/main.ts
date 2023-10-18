7c97c90aaf53b895059c852049dee3183932d59b
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-10-17 18:23:55 -0700

    Changed to use as component

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,16 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { registerPlugins } from './plugins';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import VColorPickerField from './index';
+import { VColorPickerField } from './index';
 
 
 const app = createApp(App);
-app.use(VColorPickerField);
+
 app.component('CodeBlock', CodeBlock);
 app.use(createPinia());
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.component('FaIcon', FontAwesomeIcon);
+app.component('VColorPickerField', VColorPickerField);
 
 registerPlugins(app);
 
