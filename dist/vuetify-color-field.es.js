2a75cd6d699ed088460d1e971baa7e97248f4300
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-01-10 10:29:34 -0800

    Build

--- dist/vuetify-color-field.es.js
+++ dist/vuetify-color-field.es.js
@@ -2,7 +2,7 @@ import { Fragment as be, reactive as bt, computed as v, watchEffect as _t, toRef
 import { onClickOutside as ba } from "@vueuse/core";
 /**
  * @name @wdns/vuetify-color-field
- * @version 1.1.0
+ * @version 1.1.1
  * @description Vuetify Color Field is a Vuetify VTextField Color Picker Component
  * @author WebDevNerdStuff & Bunnies... lots and lots of bunnies! <webdevnerdstuff@gmail.com> (https://webdevnerdstuff.com)
  * @copyright Copyright 2024, WebDevNerdStuff
